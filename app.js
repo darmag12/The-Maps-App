6e41af005894bed8f28ea99aa521b0e13494571a
Daryl Magera <DarylMagera@global5.local>
2021-09-02 15:26:17 -0400

    the map is loading on the page now

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let appKey = 'AIzaSyBXlrpB1sbqwFV3Ka2r1dYvgGoyPDLlGC4';
 let containerId = document.getElementById('map');
+let map;
 
 // Created the script tag & set the appropriate attributes
 let script = document.createElement('script');
@@ -9,5 +10,17 @@ script.async = true;
 // Attached callback function to the `window` object
 window.initMap = function () {
     // JS API is loaded and available
+    let options = {
+        center: {
+            lat: 28.6876424,
+            lng: -81.4019766
+        },
+        zoom: 10
+    };
+
+    map = new google.maps.Map(containerId, options);
 };
+
+// Appended the 'script' element to 'head'
+document.head.appendChild(script);
 
