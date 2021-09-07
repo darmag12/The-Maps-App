6d389436e24f7031741a8dea7155b3838610f180
Daryl Magera <DarylMagera@global5.local>
2021-09-07 17:17:30 -0400

    added a show and hide listings functionality and custom markers on the map

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let appKey = 'AIzaSyBXlrpB1sbqwFV3Ka2r1dYvgGoyPDLlGC4';
 
 // Contains All the DOM elements
 const domElements = {
-    mapContainer: document.getElementById('map'),
-    showListings: document.getElementById('show-listings'),
-    hideListings: document.getElementById('hide-listings')
+    mapContainerStr: document.getElementById('map'),
+    showListingsStr: document.getElementById('show-listings'),
+    hideListingsStr: document.getElementById('hide-listings')
 }
 
 let map;
@@ -13,32 +13,35 @@ let locations;
 let marker;
 let infoWindow;
 let bounds;
-const global5 = {lat: 28.6876424, lng: -81.4019766};
+const orlandoCod = {lat: 28.5384, lng: -81.3789};
 
 // Created the script tag & set the appropriate attributes
 let script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=${appKey}&callback=initMap`;
 script.async = true;
+script.src = `https://maps.googleapis.com/maps/api/js?key=${appKey}&callback=initMap`;
+
 
 // Attached callback function to the `window` object
 window.initMap = function () {
+    let markers = [];
+
     // JS API is loaded and available
     let options = {
-        center: global5,
+        center: orlandoCod,
         zoom: 13,
-        mapId: '9ffa16729c1a3c66'
+        // mapId: '9ffa16729c1a3c66'
     };
 
-    map = new google.maps.Map(domElements.mapContainer, options);
+    map = new google.maps.Map(domElements.mapContainerStr, options);
 
     locations = [
-        {title: 'I-4 Eastbound Rest Area', location: {lat: 28.7018043, lng: -81.402416}},
-        {title: 'Polk County Rest Area I-4 Westbound', location:{lat: 28.177376, lng:-81.794276}},
+        {title: 'I-4 Westbound Rest Area', location: {lat: 28.703015, lng: -81.3869928}},
+        {title: 'Polk County Rest Area I-4 Westbound', location:{lat: 28.172558, lng:-81.767027}},
+        {title: 'Polk County Rest Area I-4 Eastbound', location:{lat: 28.1676031, lng:-81.7730702}},
         {title: 'Love\'s Travel Stop', location:{lat: 28.1551445, lng: -81.8186561}}
     ]
 
     infoWindow = new google.maps.InfoWindow();
-    bounds = new google.maps.LatLngBounds();
 
     // loop through each location and create a new marker for each one
     locations.forEach((location, i) => {
@@ -53,26 +56,31 @@ window.initMap = function () {
 
         // new marker instance
         marker = new google.maps.Marker({
-        map: map,
         position: position,
+        label: {
+            text: "\ue63d", // always start with \u then the icon code
+            fontFamily: "Material Icons",
+            color: "#ffffff",
+            className: "rest-area-icons",
+            fontSize: "18px",
+          },
         title: title,
         animation: google.maps.Animation.DROP,
         id: i
     });
 
-    // Extends the boundaries of the map for each marker
-    bounds.extend(marker.position);
+    // Push each marker into the markers array
+    markers.push(marker);
 
     // Added click event listener on the marker
     marker.addListener('click', function(){
       populateInfoWindow(this, infoWindow)
     });
 
-    map.fitBounds(bounds);
-
-    domElements.showListings
-
     }); // end of forEach loop
+
+    domElements.showListingsStr.addEventListener('click', showListings);
+    domElements.hideListingsStr.addEventListener('click', hideListings);
 
     function populateInfoWindow(marker, infoWind){
         if(infoWind.marker != marker){
@@ -81,6 +89,27 @@ window.initMap = function () {
             infoWind.open(map, marker);
         }
     }
+
+    function showListings() {
+        // created a new instance of the boundary object
+        bounds = new google.maps.LatLngBounds();
+        // Extends the boundaries of the map for each marker
+        markers.forEach(mark => {
+            // console.log(mark);
+            mark.setMap(map);
+            bounds.extend(mark.position);
+    
+        });
+        map.fitBounds(bounds);
+
+    }
+
+    function hideListings() {
+        markers.forEach(mark => {
+            mark.setMap(null);
+        });
+    }
+
 
 };
 
